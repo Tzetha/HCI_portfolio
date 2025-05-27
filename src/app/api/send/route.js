8df6bdcc8963776b5ade27c7b6cffea4697da81c
Ethan <ethangabriel.soncio@wvsu.edu.ph>
2025-05-27 17:35:01 +0800

    modified route

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,5 +1,3 @@
-import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
